835d4d1742cd819b1f9a0dc2e98eca22d2283561
woominsik <9737638@naver.com>
2022-10-04 14:15:48 +0900

    시크릿 키가 존재해야 한다.

--- src/main/java/com/ll/exam/app__2022_10_04/App20221004Application.java
+++ src/main/java/com/ll/exam/app__2022_10_04/App20221004Application.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app__2022_10_04;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
